8283fc732ee510f61be2d01727bf2cf77dfc6653
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-11 16:55:39 +0100

    imported routes and wrote logout & register & logout function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Order from "./pages/Order"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+import Home from "./pages/Home"
+import ProtectedRoute from "./components/ProtectedRoute"
+
+// write logout function
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+// write register and logout function
+function RegisterAndLogout(){
+  localStorage.clear()
+  return <Register />
+}
+
 function App() {
 
   return (
