a94d6b7eed10905999b37f4f7fa79511ec620392
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-11 15:25:15 +0100

    navigate user to login if not authorized

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -6,9 +6,18 @@ import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
 import { useState, useEffect } from 'react'
 
 // Protected route component for only authorized users
+// eslint-disable-next-line react/prop-types
 function ProtectedRoute({children}) {
     // create state to track authorization status
     const [isAuthorized, setIsAuthorized] = useState(null)
+
+    // until state is not null, render this div 
+    if (isAuthorized === null) {
+        return <div>Loading...</div>
+    }
+
+    // return children if authorized or navigate to login page
+    return isAuthorized ? children : <Navigate to="/login" />
 
 }
 
