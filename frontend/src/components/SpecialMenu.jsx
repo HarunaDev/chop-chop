a64b864e65c1c2112b8f0f8b21ae4d368a215ebb
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-27 22:25:56 +0100

    modidifed special menu comp to style the section container & h1 element

--- frontend/src/components/SpecialMenu.jsx
+++ frontend/src/components/SpecialMenu.jsx
@@ -1,7 +1,7 @@
 function SpecialMenu() {
     return (
-        <section id="special-section">
-            <h1>Checkout our Special Combo Menu</h1>
+        <section id="special-section" className="h-[600px] bg-[] border-2 border-[red] py-16 px-16 bg-[#F8E44D]">
+            <h1 className="text-center w-[400px] mx-auto text-4xl text-[#282828] font-bold">Checkout our Special Combo Menu</h1>
         </section>
     )
 }
