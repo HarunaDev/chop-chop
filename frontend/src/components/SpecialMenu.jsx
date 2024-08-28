cf0c8269d179fd2328ee842b6b27d27e634dd986
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-28 10:14:01 +0100

    rendered menu cards

--- frontend/src/components/SpecialMenu.jsx
+++ frontend/src/components/SpecialMenu.jsx
@@ -1,7 +1,16 @@
+import MenuCard from "./MenuCard"
+import BurgerSmoothie from "../assets/burger&smoothie.png"
+import FriesSpecial from "../assets/fries-special-combo.png"
+import FriesChickenSmoothie from "../assets/fries-chicken-smoothie.png"
+
 function SpecialMenu() {
     return (
-        <section id="special-section" className="h-[600px] bg-[] border-2 border-[red] py-16 px-16 bg-[#F8E44D]">
-            <h1 className="text-center w-[400px] mx-auto text-4xl text-[#282828] font-bold">Checkout our Special Combo Menu</h1>
+        <section id="special-section" className="h-auto bg-[] border-2 border-[red] py-16 px-16 bg-[#F8E44D]">
+            <h1 className="text-center w-[400px] mx-auto text-4xl text-[#282828] font-bold mb-16">Checkout our Special Combo Menu</h1>
+
+            <MenuCard detail="3pcs Chicken & Fries" img={FriesSpecial} />
+            <MenuCard detail="1pc Big Burger & Smoothie" img={BurgerSmoothie} />
+            <MenuCard detail="6pcs Chicken & Fries with Smoothie" img={FriesChickenSmoothie} />
         </section>
     )
 }
