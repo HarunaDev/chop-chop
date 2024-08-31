76c3d7e1b196f9ddc7f6da6c6189104cc375b451
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-01 00:22:28 +0100

    rendered nav bar in order page

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,5 +1,31 @@
+import NavBar from "../components/NavBar"
+import { useState, useEffect } from "react"
+import api from "../api"
+
 function Order() {
-    return <div>Order here</div>
+    // state to hold user
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    // function to make api call and get user
+    const getUser = () =>{
+        api
+        .get("/api/user/")
+        .then((res) => res.data)
+        .then((data) => {
+            setUser(data.username)
+        })
+        .catch((err) => alert(err))
+    }
+
+    return (
+        <>
+            <NavBar user={user} />
+        </>
+    )
 }
 
 export default Order
