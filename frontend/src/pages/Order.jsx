ab6db3bc5ba3661a566a683137464a624ce9cd20
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-03 02:45:46 +0100

    modified order page to take components with different props

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -1,8 +1,10 @@
 import NavBar from "../components/NavBar"
 import { useState, useEffect } from "react"
 import api from "../api"
+import { motion } from "framer-motion"
 import { Link } from 'react-router-dom'
 import Banner from "../components/Banner"
+import Footer from "../components/Footer"
 
 function Order() {
     // state to hold user
@@ -27,12 +29,17 @@ function Order() {
         <>
             <NavBar user={user} url="/" text="Home" />
             <Banner orderUrl="#Menu" orderText="Go to Menu" banner={
-                <div>
-                    <h1>Welcome to Choji chop shop</h1>
+                
                     <Link to="">
-                        <button>Create your order</button>
+                        <motion.button className="mx-auto border-4 border-[green] p-8 rounded text-4xl font-bold text-[green]"
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1 }}
+                        transition={{ duration: 0.5 }}
+                         whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.9 }}>Create your order</motion.button>
                     </Link>
-                </div>} />
+                } />
+            <Footer />
         </>
     )
 }
